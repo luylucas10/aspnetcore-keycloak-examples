1ca4f063586c2c785e3ff41f8f1638145a37bedf
luylucas10 <luylucas10@gmail.com>
2024-01-21 17:38:42 -0300

    MVC
    Adição do projeto MVC;
    Integração do MVC com Keycloak;

---
+++ src/Keycloak.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Keycloak.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Keycloak.Mvc.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Authorize]
+		public IActionResult Privacy()
+		{
+			return Json(HttpContext.User, new JsonSerializerOptions(){ReferenceHandler = ReferenceHandler.IgnoreCycles});
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ src/Keycloak.Mvc/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Keycloak.Mvc
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddAuthentication(options =>
+				{
+					options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+					options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+				})
+				.AddCookie(opt =>
+				{
+					opt.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+				})
+				.AddOpenIdConnect(options =>
+				{
+					options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+					options.Authority = $"{builder.Configuration["Keycloak:auth-server-url"]}realms/{builder.Configuration["Keycloak:realm"]}";
+					options.ClientId = builder.Configuration["Keycloak:resource"];
+					options.ResponseType = OpenIdConnectResponseType.CodeIdTokenToken;
+					options.UsePkce = true;
+					options.Scope.Add("profile");
+					options.SaveTokens = true;
+					options.GetClaimsFromUserInfoEndpoint = true;
+					options.RequireHttpsMetadata = false;
+					options.TokenValidationParameters = new TokenValidationParameters() { RoleClaimType = "roles" };
+					//options.Events.OnTicketReceived = context =>
+					//{
+					//	var t = context.Properties.GetTokens().ToList();
+					//	return Task.CompletedTask;
+					//};
+				});
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
+}
