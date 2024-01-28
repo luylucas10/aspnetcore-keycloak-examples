0e2034b7d36f7e0e95d21010d7bd23512ff775d0
luylucas10 <luylucas10@gmail.com>
2024-01-28 13:01:38 -0300

    API
    Ajustes no app settings;
    Comentários de documentação

--- src/Keycloak.Api/Program.cs
+++ src/Keycloak.Api/Program.cs
@@ -1,76 +1,82 @@
-
 using Keycloak.AuthServices.Authentication;
 using Keycloak.AuthServices.Authorization;
 using Microsoft.OpenApi.Models;
 
-namespace Keycloak.Api
+namespace Keycloak.Api;
+
+public class Program
 {
-	public class Program
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
+		var builder = WebApplication.CreateBuilder(args);
+
+		builder.Services.AddKeycloakAuthentication(new KeycloakAuthenticationOptions()
 		{
-			var builder = WebApplication.CreateBuilder(args);
-			builder.Services.AddKeycloakAuthentication(new KeycloakAuthenticationOptions()
-			{
-				AuthServerUrl = builder.Configuration["Keycloak:auth-server-url"]!,
-				Realm = builder.Configuration["Keycloak:realm"]!,
-				SslRequired = builder.Configuration["Keycloak:ssl-required"]!,
-				VerifyTokenAudience = false
-			});
+			// Keycloak server URL
+			AuthServerUrl = builder.Configuration["Keycloak:auth-server-url"]!,
+			// Realm Name
+			Realm = builder.Configuration["Keycloak:realm"]!,
+			// ClientId
+			Resource = builder.Configuration["Keycloak:resource"]!,
 
-			builder.Services.AddKeycloakAuthorization(new KeycloakProtectionClientOptions()
-			{
-				AuthServerUrl = builder.Configuration["Keycloak:auth-server-url"]!,
-				Realm = builder.Configuration["Keycloak:realm"]!,
-				SslRequired = builder.Configuration["Keycloak:ssl-required"]!,
-				VerifyTokenAudience = false
-			});
+			SslRequired = builder.Configuration["Keycloak:ssl-required"]!,
+			VerifyTokenAudience = false,
+		});
 
-			builder.Services.AddControllers();
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen(c =>
+		builder.Services.AddKeycloakAuthorization(new KeycloakProtectionClientOptions()
+		{
+			AuthServerUrl = builder.Configuration["Keycloak:auth-server-url"]!,
+			Realm = builder.Configuration["Keycloak:realm"]!,
+			Resource = builder.Configuration["Keycloak:resource"]!,
+			SslRequired = builder.Configuration["Keycloak:ssl-required"]!,
+			VerifyTokenAudience = false
+		});
+
+		builder.Services.AddControllers();
+		builder.Services.AddEndpointsApiExplorer();
+
+		builder.Services.AddSwaggerGen(c =>
+		{
+			var securityScheme = new OpenApiSecurityScheme
 			{
-				var securityScheme = new OpenApiSecurityScheme
+				Name = "Keycloak",
+				In = ParameterLocation.Header,
+				Type = SecuritySchemeType.OpenIdConnect,
+				OpenIdConnectUrl = new Uri($"{builder.Configuration["Keycloak:auth-server-url"]}realms/{builder.Configuration["Keycloak:realm"]}/.well-known/openid-configuration"),
+				Scheme = "bearer",
+				BearerFormat = "JWT",
+				Reference = new OpenApiReference
 				{
-					Name = "Keycloak",
-					In = ParameterLocation.Header,
-					Type = SecuritySchemeType.OpenIdConnect,
-					OpenIdConnectUrl = new Uri($"{builder.Configuration["Keycloak:auth-server-url"]}realms/{builder.Configuration["Keycloak:realm"]}/.well-known/openid-configuration"),
-					Scheme = "bearer",
-					BearerFormat = "JWT",
-					Reference = new OpenApiReference
-					{
-						Id = "Bearer",
-						Type = ReferenceType.SecurityScheme
-					}
-				};
-				c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
-				c.AddSecurityRequirement(new OpenApiSecurityRequirement
-				{
-					{securityScheme, Array.Empty<string>()}
-				});
-			});
-
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
+					Id = "Bearer",
+					Type = ReferenceType.SecurityScheme
+				}
+			};
+			c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+			c.AddSecurityRequirement(new OpenApiSecurityRequirement
 			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+				{securityScheme, Array.Empty<string>()}
+			});
+		});
 
-			app.UseHttpsRedirection();
+		var app = builder.Build();
 
-      app.UseCors(a => a.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-
-			app.UseAuthentication();
-			app.UseAuthorization();
-
-
-			app.MapControllers();
-
-			app.Run();
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
 		}
+
+		app.UseHttpsRedirection();
+
+		app.UseCors(a => a.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+		app.UseAuthentication();
+		app.UseAuthorization();
+
+
+		app.MapControllers();
+
+		app.Run();
 	}
 }
